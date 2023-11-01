9b8a10e231d0df957e7b30fd7aed36fb494d665a
virgonias <yandaffa19@gmail.com>
2023-11-01 22:25:24 +0700

    menambah dan mengganti program

---
+++ Perkenalan.java
@@ -0,0 +1,15 @@
+public class Perkenalan {
+    public static void main(String[] args) {
+        // Nama
+        System.out.println("yan Daffa Putra Liandhie");
+
+        // Kelas
+        System.out.println("1D");
+
+        // Harapan
+        System.out.println("Bisa menjadi web developer.");
+
+        //jokes
+        System.out.println("pernah kepikiran ga sih cara godzila reproduksi?");
+    }
+}
