96329ca1b6a1b5c4ee1e7d86827b340efec77607
virgonias <yandaffa19@gmail.com>
2023-11-01 20:59:56 +0700

    menambah

--- pertemuan1.java
+++ pertemuan1.java
@@ -4,6 +4,11 @@ public class pertemuan1 {
         System.out.println("Hello word");
         System.out.println("hello world 2");
         System.out.println("mendambahkan jurusan");
+        System.out.println("say punya teman bernama hehe")
+        System.out.println("say punya teman bernama hehe2")
+        System.out.println("say HI")
+        
+        
         
     }
 }
